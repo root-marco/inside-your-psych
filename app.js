bb550dc2d710648106ceaa86d5ed700afaeb68f9
chaos <rootslowed@gmail.com>
2021-01-19 19:22:02 -0300

    fix bodyparser.json() bug // adding adminRouter

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import bodyParser from 'body-parser';
+import adminRouter from './routes/admin.js';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.JSON);
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+	app.set('view engine', 'handlebars');
+	app.use('/admin', adminRouter);
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-app.listen(port, () => {
-	console.log(`server listen on port ${port}`);
-});
+app.listen(port, () => console.log(`server running`));
