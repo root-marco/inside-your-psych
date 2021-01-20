456a466d7c05ce42383a428b90853f90171b6c33
chaos <rootslowed@gmail.com>
2021-01-20 17:06:38 -0300

    mongoose config done and adding comments

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+/* MODULES */
 import express from 'express';
 import handlebars from 'express-handlebars';
 import bodyParser from 'body-parser';
 import path from 'path';
+import mongoose from 'mongoose';
 import adminRouter from './routes/admin.js';
+/* ----- */
 
-const __dirname = path.resolve();
 const app = express();
 const port = 3000;
+const db = mongoose.connection;
+
+/* CONFIGS */
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/inside-your-psych', { useNewUrlParser: true, useUnifiedTopology: true });
+db.on('open', () => console.log('database connected'));
+db.on('error', console.error.bind(console, 'connection error'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,8 +23,11 @@ app.use(bodyParser.json());
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(`${__dirname}public`)));
+app.use(express.static(path.resolve('public')));
+/* ----- */
 
+/* ROUTES */
 app.use('/admin', adminRouter);
+/* ----- */
 
 app.listen(port, () => console.log(`server running`));
