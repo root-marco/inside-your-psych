3692ef5d38038e4f898abb54dcbc62a031819956
chaos <rootslowed@gmail.com>
2021-01-20 17:07:20 -0300

    render admin categories and addcategories

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,11 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categories', (req, res) => {
-	res.send('<h1>Categories page</h1>');
+	res.render('admin/categories');
+});
+
+router.get('/categories/add', (req, res) => {
+	res.render('admin/addcategorie');
 });
 
 export default router;
