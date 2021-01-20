66c6b7c4fc896c5426787005ee3d1e27798490c3
chaos <rootslowed@gmail.com>
2021-01-20 11:30:45 -0300

    removing whitespaces

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ import adminRouter from './routes/admin.js';
 const app = express();
 const port = 3000;
 
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
-	app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
-	app.set('view engine', 'handlebars');
-	app.use('/admin', adminRouter);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use('/admin', adminRouter);
 
 app.listen(port, () => console.log(`server running`));
