cb0526ef5a9e7418b5d3fb3cb2c20bd6dbead4c0
chaos <rootslowed@gmail.com>
2021-01-20 12:15:26 -0300

    changing the way to use bootstrap

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('<h1>Main page - ADMIN PANEL</h1>');
+	res.render('admin/index');
 });
 
 router.get('/posts', (req, res) => {
