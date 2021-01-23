6c49a73485db3f7343f9cd7525c75a9dc125efce
chaos <rootslowed@gmail.com>
2021-01-23 15:39:17 -0300

    controller implemented

--- routes/admin.js
+++ routes/admin.js
@@ -1,37 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import category from '../models/Category.js';
+import * as admin from '../controllers/admin.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.render('admin/index');
-});
-
-router.get('/posts', (req, res) => {
-	res.send('<h1>Posts page</h1>');
-});
-
-router.get('/categories', async (req, res) => {
-	res.render('admin/categories');
-});
-
-
-router.get('/categories/add', (req, res) => {
-	res.render('admin/addcategory');
-});
-
-router.post('/categories/new', (req, res) => {
-	const newCategory = {
-		name: req.body.name,
-		slug: req.body.slug,
-	};
-
-	new category(newCategory).save()
-	.then(() => console.log('category added'))
-	.catch((err) => console.log('error'));
-
-	res.redirect('/admin/categories');
-});
+router.get('/', admin.root);
+router.get('/posts', admin.posts);
+router.get('/categories', admin.categories);
+router.get('/categories/add', admin.categoriesAdd);
+router.post('/categories/new', admin.categoriesNew);
 
 export default router;
