837255241c49ff6e8a4381f2c59e50baa81d8919
chaos <rootslowed@gmail.com>
2021-01-22 22:26:44 -0300

    changing the Category Schame

--- models/Category.js
+++ models/Category.js
@@ -1,18 +1,9 @@
 import mongoose from 'mongoose';
 
 const Category = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	slug: {
-		type: String,
-		required: true,
-	},
-	date: {
-		type: Date,
-		default: Date.now(),
-	},
+	name: { type: String, required: true },
+	slug: { type: String, required: true },
+	date: { type: Date, default: Date.now() },
 });
 
 const category = mongoose.model('category', Category);
