1537903ee11cb81285f9373e565be6451f7b5b7f
chaos <rootslowed@gmail.com>
2021-01-25 14:30:34 -0300

    router get - categoriees edit by id

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,8 @@ router.get('/', admin.root);
 router.get('/posts', admin.posts);
 router.get('/categories', admin.categories);
 router.get('/categories/add', admin.categoriesAdd);
+router.get('/categories/edit/:id', admin.categoriesEdit);
+
 router.post('/categories/new', admin.categoriesNew);
 
 export default router;
