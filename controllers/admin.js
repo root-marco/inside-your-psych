cb3d94aea4576ad0d6d206ef58bd9d75593e5bd5
chaos <rootslowed@gmail.com>
2021-01-25 16:58:37 -0300

    logic to delete an category by id

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,8 +10,12 @@ export const posts = async (req, res) => {
 
 export const categories = async (req, res) => {
 
-	category.find().sort({_id:-1}).lean().then((categories) => {
-		res.render('admin/categories', { categories: categories });
+	category.find().sort({
+		_id: -1
+	}).lean().then((categories) => {
+		res.render('admin/categories', {
+			categories: categories
+		});
 	}).catch((err) => {
 		req.flash('error_msg', 'unable to list categories');
 		res.redirect('/admin');
@@ -27,27 +31,49 @@ export const categoriesNew = async (req, res) => {
 	const name = req.body.name;
 	const slug = req.body.slug;
 
-	if ( !name || typeof name == undefined || name == null)
-	errors.push({ text: 'Invalid name' });
+	if (!name || typeof name == undefined || name == null)
+		errors.push({
+			text: 'Invalid name'
+		});
 
-	if ( !slug || typeof slug == undefined || slug == null)
-	errors.push({ text: 'Invalid slug' });
+	if (!slug || typeof slug == undefined || slug == null)
+		errors.push({
+			text: 'Invalid slug'
+		});
 
 	if (errors.length > 0) {
-		res.render('admin/categoriesadd', { errors: errors });
+		res.render('admin/categoriesadd', {
+			errors: errors
+		});
 	} else {
 		const newCategory = {
 			name: name,
 			slug: slug,
 		};
-	
+
 		new category(newCategory).save()
-		.then(() => {
-			req.flash('success_msg', 'successfully created category')
-			res.redirect('/admin/categories');
-		}).catch((err) => {
-			req.flash('error_msg', 'error creating category')
-			console.log('error')
-		});
+			.then(() => {
+				req.flash('success_msg', 'successfully created category')
+				res.redirect('/admin/categories');
+			}).catch((err) => {
+				req.flash('error_msg', 'error creating category')
+				console.log('error')
+			});
 	}
-}
+};
+
+export const categoriesDelete = async (req, res) => {
+	const id = req.params.id;
+
+	category.deleteOne({
+		_id: id
+	}, (err) => {
+		if (!err) {
+			console.log('boa')
+		} else {
+			console.log(err)
+		}
+	});
+
+	res.redirect('/admin/categories');
+};
