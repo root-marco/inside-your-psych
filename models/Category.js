ba263b410ee7a4c48acf25b0b74fdc6579a0dd73
chaos <rootslowed@gmail.com>
2021-01-25 16:58:53 -0300

    making code more readable

--- models/Category.js
+++ models/Category.js
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
 
 const Category = new mongoose.Schema({
-	name: { type: String, required: true },
-	slug: { type: String, required: true },
-	date: { type: Date, default: Date.now() },
+	name: {
+		type: String,
+		required: true
+	},
+	slug: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now()
+	},
 });
 
 const category = mongoose.model('category', Category);
