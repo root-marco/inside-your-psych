ec99a7bb9b322f0a4579bf7966f97150152f56cd
chaos <rootslowed@gmail.com>
2021-01-26 16:11:13 -0300

    postsadd route implemented

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,14 @@ import * as admin from '../controllers/admin.js';
 const router = express.Router();
 
 router.get('/', admin.root);
+
 router.get('/posts', admin.posts);
+router.get('/posts/add', admin.postsAdd);
+
 router.get('/categories', admin.categories);
 router.get('/categories/add', admin.categoriesAdd);
 router.get('/categories/edit/:id', admin.categoriesEditId);
 router.get('/categories/delete/:id', admin.categoriesDeleteId);
-
 router.post('/categories/new', admin.categoriesNew);
 router.post('/categories/edit', admin.categoriesEdit);
 
