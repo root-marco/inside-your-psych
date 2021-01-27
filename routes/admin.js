431e3c086d336aae2e43d51cc217e01ee651ea64
chaos <rootslowed@gmail.com>
2021-01-27 20:21:32 -0300

    posts new route implemented

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ router.get('/', admin.root);
 
 router.get('/posts', admin.posts);
 router.get('/posts/add', admin.postsAdd);
+router.get('/posts/new', admin.postsNew);
 
 router.get('/categories', admin.categories);
 router.get('/categories/add', admin.categoriesAdd);
