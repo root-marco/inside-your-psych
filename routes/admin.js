976f5edd114fa638220f0dee4a1564747e7b3298
chaos <rootslowed@gmail.com>
2021-02-02 15:52:17 -0300

    fix postsEdit route

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get('/', admin.root);
 router.get('/posts', admin.posts);
 router.get('/posts/add', admin.postsAdd);
 router.post('/posts/new', admin.postsNew);
-router.post('/posts/edit', admin.categoriesEdit);
+router.post('/posts/edit', admin.postsEdit);
 router.get('/posts/edit/:id', admin.postsEditId);
 router.get('/posts/delete/:id', admin.postsDeleteId);
 
