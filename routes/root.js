df989b412b98ef8fe2a1e41d7d320261ded257c6
chaos <rootslowed@gmail.com>
2021-02-04 16:46:51 -0300

    categories route implemented

--- routes/root.js
+++ routes/root.js
@@ -4,7 +4,8 @@ import * as root from '../controllers/root.js';
 const router = express.Router();
 
 router.get('/', root.root);
-router.get('/404', root.error404);
 router.get('/post/:slug', root.postSlug);
+router.get('/categories', root.categories);
+router.get('/404', root.error404);
 
 export default router;
