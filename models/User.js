e4a39672782f1aa29b12881b2455460398a29b76
Marco <rootslowed@gmail.com>
2021-02-05 15:47:38 -0300

    mongoose model User created

---
+++ models/User.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+const user = mongoose.model('user', User);
+
+export default user;
