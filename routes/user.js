b1268808f5e9510c70416ec8ec924e26f7889e79
Marco <rootslowed@gmail.com>
2021-02-06 12:36:11 -0300

    login route created

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ import * as user from '../controllers/user.js';
 const router = express.Router();
 
 router.get('/register', user.register);
+router.get('/login', user.login);
 router.post('/register', user.registerNew);
 
 export default router;
