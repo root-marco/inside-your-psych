edecc238585bebdb5fa1816e73e272e9e43313cc
Marco <rootslowed@gmail.com>
2021-02-08 15:40:40 -0300

    global variable to show error created

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(flash());
 app.use((req, res, next) => {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
 	next();
 });
 
