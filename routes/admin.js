4d26c65d407a98451e286a1c34b4a3cbfd0d85c1
Marco <rootslowed@gmail.com>
2021-02-08 17:30:03 -0300

    isAdmin middleware implemented in all routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import * as admin from '../controllers/admin.js';
+import isAdmin from '../helpers/isAdmin.js'
 
 const router = express.Router();
 
-router.get('/', admin.admin);
+router.get('/', isAdmin, admin.admin);
 
-router.get('/posts', admin.posts);
-router.get('/posts/add', admin.postsAdd);
-router.post('/posts/new', admin.postsNew);
-router.post('/posts/edit', admin.postsEdit);
-router.get('/posts/edit/:id', admin.postsEditId);
-router.get('/posts/delete/:id', admin.postsDeleteId);
+router.get('/posts', isAdmin, admin.posts);
+router.get('/posts/add', isAdmin, admin.postsAdd);
+router.post('/posts/new', isAdmin, admin.postsNew);
+router.post('/posts/edit', isAdmin, admin.postsEdit);
+router.get('/posts/edit/:id', isAdmin, admin.postsEditId);
+router.get('/posts/delete/:id', isAdmin, admin.postsDeleteId);
 
-router.get('/categories', admin.categories);
-router.get('/categories/add', admin.categoriesAdd);
-router.post('/categories/new', admin.categoriesNew);
-router.post('/categories/edit', admin.categoriesEdit);
-router.get('/categories/edit/:id', admin.categoriesEditId);
-router.get('/categories/delete/:id', admin.categoriesDeleteId);
+router.get('/categories', isAdmin, admin.categories);
+router.get('/categories/add', isAdmin, admin.categoriesAdd);
+router.post('/categories/new', isAdmin, admin.categoriesNew);
+router.post('/categories/edit', isAdmin, admin.categoriesEdit);
+router.get('/categories/edit/:id', isAdmin, admin.categoriesEditId);
+router.get('/categories/delete/:id', isAdmin, admin.categoriesDeleteId);
 
 export default router;
