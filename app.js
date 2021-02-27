2db9b00b6bf30296b67748b415ee28655fabaefe
marco <rootslowed@gmail.com>
2021-02-26 21:43:13 -0300

    adding some coomments

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-import express from 'express';
+// MODULES
+import dotenv from 'dotenv'; dotenv.config();
 import handlebars from 'express-handlebars';
-import bodyParser from 'body-parser';
-import path from 'path';
-import mongoose from 'mongoose';
 import session from 'express-session';
+import bodyParser from 'body-parser';
 import flash from 'connect-flash';
-import dotenv from 'dotenv';
-dotenv.config();
-
-import adminRouter from './routes/admin.js';
-import rootRouter from './routes/root.js';
-import userRouter from './routes/user.js';
-
-import configAuth from './config/auth.js';
+import mongoose from 'mongoose';
 import passport from 'passport';
-configAuth(passport);
+import express from 'express';
+import path from 'path';
 
+// APP
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.resolve('public')));
 
+// SESSION
 app.use(session({
-	secret: '0152',
+	secret: process.env.SECRET,
 	resave: true,
 	saveUninitialized: true,
 }));
 
+// PASSPORT
+import configAuth from './config/auth.js';
 app.use(passport.initialize());
 app.use(passport.session());
+configAuth(passport);
 
+// FLASH
 app.use(flash());
-
 app.use((req, res, next) => {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
@@ -38,30 +37,38 @@ app.use((req, res, next) => {
 	next();
 });
 
+// BODY PARSER
 app.use(bodyParser.urlencoded({
 	extended: true,
 }));
 app.use(bodyParser.json());
 
+// HANDLEBARS
 app.engine('handlebars', handlebars({
 	defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.resolve('public')));
+// ROUTES
+import rootRouter from './routes/root.js';
+app.use('/', rootRouter);
+import adminRouter from './routes/admin.js';
+app.use('/admin', adminRouter);
+import userRouter from './routes/user.js';
+app.use('/user', userRouter);
 
+// MONGOOSE
 try {
-	await mongoose.connect(process.env.mongoURI, {
+	await mongoose.connect(process.env.MONGOURI, {
 		useUnifiedTopology: true,
 		useNewUrlParser: true,
 	});
 	console.log('database connected');
-} catch(error) {
+} catch (error) {
 	console.log(error);
 }
 
-app.use('/user', userRouter);
-app.use('/admin', adminRouter);
-app.use('/', rootRouter);
-
-app.listen(port, () => console.log(`server running`));
+// LISTEN
+app.listen(PORT, () => {
+	console.log(`server running on localhost:${PORT}`);
+});
