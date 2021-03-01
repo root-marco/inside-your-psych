8eaaca3fab3883212472bf7bf4c76f455ca26d94
marco <rootslowed@gmail.com>
2021-03-01 19:26:40 -0300

    delete method implemented to posts and categories

--- routes/admin.js
+++ routes/admin.js
@@ -6,18 +6,24 @@ const router = express.Router();
 
 router.get('/', isAdmin, admin.admin);
 
+// POSTS
 router.get('/posts', isAdmin, admin.posts);
 router.get('/posts/add', isAdmin, admin.postsAdd);
+
 router.post('/posts/new', isAdmin, admin.postsNew);
 router.post('/posts/edit', isAdmin, admin.postsEdit);
-router.get('/posts/edit/:id', isAdmin, admin.postsEditId);
-router.get('/posts/delete/:id', isAdmin, admin.postsDeleteId);
 
+router.get('/posts/edit/:id', isAdmin, admin.postsEditId);
+router.delete('/posts/delete/:id', isAdmin, admin.postsDeleteId);
+
+// CATEGORIES
 router.get('/categories', isAdmin, admin.categories);
 router.get('/categories/add', isAdmin, admin.categoriesAdd);
+
 router.post('/categories/new', isAdmin, admin.categoriesNew);
 router.post('/categories/edit', isAdmin, admin.categoriesEdit);
+
 router.get('/categories/edit/:id', isAdmin, admin.categoriesEditId);
-router.get('/categories/delete/:id', isAdmin, admin.categoriesDeleteId);
+router.delete('/categories/delete/:id', isAdmin, admin.categoriesDeleteId);
 
 export default router;
