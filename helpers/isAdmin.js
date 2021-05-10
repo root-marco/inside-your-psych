cd31a5b082fd9fcebe4d92501695df3e630d8479
slowed <rootslowed@gmail.com>
2021-05-10 16:18:07 -0300

    isAdmin middleware done

--- helpers/isAdmin.js
+++ helpers/isAdmin.js
@@ -1,8 +1,12 @@
 const isAdmin = (req, res, next) => {
-	if (req.isAuthenticated() && req.user.isAdmin == 1) return next();
 
-	req.flash('error_msg', 'You\`re not admin');
+	if (req.isAuthenticated() && req.user.isAdmin === 1) {
+		return next();
+	}
+
+	req.flash('error_msg', 'You\'re not admin');
 	res.redirect('/');
+
 }
 
 export default isAdmin;
