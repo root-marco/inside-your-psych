b099b2cae9df066fbdfc9290c60bd5b4f465fb70
slowed <rootslowed@gmail.com>
2021-05-26 19:48:22 -0300

    middleware to file upload

---
+++ helpers/fileUpload.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, process.cwd() + '/public/covers');
+	},
+	filename: (req, file, callback) => {
+		callback(null, Date.now() + "-" + file.originalname);
+	},
+});
+
+const upload = multer({
+	storage: storage,
+}).single('upfile');
+
+export default upload;
